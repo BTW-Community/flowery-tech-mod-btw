e75be9edf1b6e6760631dbd4cbb480fe02edcf43
TheWinABagel <ash@rjohnston.net>
2025-10-16 19:00:26 -0500

    all the sounds

--- src/main/java/dev/bagel/util/BotaniaSounds.java
+++ src/main/java/dev/bagel/util/BotaniaSounds.java
@@ -3,7 +3,49 @@ package dev.bagel.util;
 import btw.util.sounds.AddonSoundRegistryEntry;
 
 public class BotaniaSounds {
-    public static void registerSounds() {}
-    public static final AddonSoundRegistryEntry LEXICON_PAGE = new AddonSoundRegistryEntry("botania:lexiconPage", 3);
-    public static final AddonSoundRegistryEntry WAY = new AddonSoundRegistryEntry("botania:way", 6);
+    public static void registerSounds() {
+        registerSound("agricarnation");
+        registerSound("airRod");
+        registerSound("altarCraft");
+        registerSound("babylonAttack");
+        registerSound("babylonSpawn");
+        registerSound("bellows");
+        registerSound("bifrostRod");
+        registerSound("blackLotus");
+        registerSound("dash");
+        registerSound("ding");
+        registerSound("divaCharm");
+        registerSound("divinationRod");
+        registerSound("doit");
+        registerSound("enchanterBlock");
+        registerSound("enchanterEnchant");
+        registerSound("endoflame");
+        registerSound("equipBauble");
+        registerSound("gaiaTrap");
+        registerSound("goldenLaurel");
+        registerSound("laputaStart");
+        registerSound("lexiconOpen");
+        registerSound("lexiconPage");
+        registerSound("lightRelay");
+        registerSound("manaBlaster");
+        registerSound("manaPoolCraft");
+        registerSound("missile");
+        registerSound("orechid");
+        registerSound("potionCreate");
+        registerSound("runeAltarCraft");
+        registerSound("runeAltarStart");
+        registerSound("spreaderFire");
+        registerSound("starcaller");
+        registerSound("terraBlade");
+        registerSound("terraformRod");
+        registerSound("terraPickMode");
+        registerSound("terrasteelCraft");
+        registerSound("thermalily");
+        registerSound("unholyCloak");
+        registerSound("way");
+    }
+
+    private static void registerSound(String id) {
+        new AddonSoundRegistryEntry("botania:" + id, 1);
+    }
 }
