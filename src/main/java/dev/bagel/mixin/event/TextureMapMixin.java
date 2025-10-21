9350534f48330a53a9dcc80b9b70b47fe46df5fc
TheWinABagel <ash@rjohnston.net>
2025-10-21 14:24:37 -0500

    fix interpolated textures \o/

--- src/main/java/dev/bagel/mixin/event/TextureMapMixin.java
+++ src/main/java/dev/bagel/mixin/event/TextureMapMixin.java
@@ -3,6 +3,7 @@ package dev.bagel.mixin.event;
 import net.minecraft.src.ResourceManager;
 import net.minecraft.src.TextureMap;
 import net.minecraftforge.client.event.TextureStitchEvent;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,9 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.botania.common.block.ModBlocks;
 import vazkii.botania.common.block.ModFluffBlocks;
 
+@Debug(export = true)
 @Mixin(TextureMap.class)
 public class TextureMapMixin {
-    @Inject(method = "loadTextureAtlas", at = @At(value = "INVOKE", target = "Ljava/util/List;clear()V", ordinal = 0, shift = At.Shift.AFTER))
+    @Inject(method = "loadTextureAtlas", at = @At(value = "INVOKE", target = "Lcom/prupe/mcpatcher/mal/tile/TileLoader;registerIcons(Lnet/minecraft/src/TextureMap;Ljava/lang/String;Ljava/util/Map;)V", shift = At.Shift.AFTER, remap = false))
     private void forge$TextureStitchEventPre(ResourceManager resourceManager, CallbackInfo ci) {
         TextureStitchEvent.Pre event = new TextureStitchEvent.Pre(((TextureMap) (Object) this));
         ModBlocks.bifrost.loadTextures(event);
--- src/main/java/vazkii/botania/client/render/block/InterpolatedIcon.java
+++ src/main/java/vazkii/botania/client/render/block/InterpolatedIcon.java
@@ -10,12 +10,7 @@
  */
 package vazkii.botania.client.render.block;
 
-import java.lang.reflect.Field;
-
-import net.minecraft.src.TextureAtlasSprite;
-import net.minecraft.src.TextureUtil;
-import net.minecraft.src.AnimationMetadataSection;
-import vazkii.botania.common.lib.LibObfuscation;
+import net.minecraft.src.*;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 
@@ -23,7 +18,7 @@ import net.fabricmc.api.EnvType;
 @Environment(EnvType.CLIENT)
 public class InterpolatedIcon extends TextureAtlasSprite {
 
-	protected int[][] interpolatedFrameData;
+	protected int[] interpolatedFrameData;
 
 	public InterpolatedIcon(String name) {
 		super(name);
@@ -48,37 +43,26 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 		int k = animationMetadata.getFrameIndex((frameCounter + 1) % j);
 
 		if(i != k && k >= 0 && k < framesTextureData.size()) {
-			int[][] aint = (int[][]) framesTextureData.get(i);
-			int[][] aint1 = (int[][]) framesTextureData.get(k);
+			int[] aint = (int[]) framesTextureData.get(i);
+			int[] aint1 = (int[]) framesTextureData.get(k);
 
 			if(interpolatedFrameData == null || interpolatedFrameData.length != aint.length)
-				interpolatedFrameData = new int[aint.length][];
+				interpolatedFrameData = new int[aint.length];
 
 			for(int l = 0; l < aint.length; l++) {
-				if (interpolatedFrameData[l] == null)
-					interpolatedFrameData[l] = new int[aint[l].length];
+				if (interpolatedFrameData == null)
+					interpolatedFrameData = new int[aint.length];
 
-				if(l < aint1.length && aint1[l].length == aint[l].length)
-					for (int i1 = 0; i1 < aint[l].length; ++i1) {
-						int j1 = aint[l][i1];
-						int k1 = aint1[l][i1];
+				for (int i1 = 0; i1 < aint.length; ++i1) {
+						int j1 = aint[i1];
+						int k1 = aint1[i1];
 						int l1 = (int) (((j1 & 16711680) >> 16) * d0 + ((k1 & 16711680) >> 16) * (1.0D - d0));
 						int i2 = (int) (((j1 & 65280) >> 8) * d0 + ((k1 & 65280) >> 8) * (1.0D - d0));
 						int j2 = (int) ((j1 & 255) * d0 + (k1 & 255) * (1.0D - d0));
-						interpolatedFrameData[l][i1] = j1 & -16777216 | l1 << 16 | i2 << 8 | j2;
+						interpolatedFrameData[i1] = j1 & -16777216 | l1 << 16 | i2 << 8 | j2;
 					}
 			}
-
-			uploadTextureMipmap(interpolatedFrameData, width, height, originX, originY);
-		}
-	}
-	//todofix all of this
-	public static void uploadTextureMipmap(int[][] p_147955_0_, int p_147955_1_, int p_147955_2_, int p_147955_3_, int p_147955_4_)
-	{
-		for (int i1 = 0; i1 < p_147955_0_.length; ++i1)
-		{
-			int[] aint1 = p_147955_0_[i1];
-			TextureUtil.uploadTextureSub(aint1, i1, p_147955_1_ >> i1, p_147955_2_ >> i1, p_147955_3_ >> i1, false/* ????? p_147955_4_ >> i1*/, p_147955_0_.length > 1);
+			TextureUtil.uploadTextureSub(interpolatedFrameData, width, height, originX, originY, false, false);
 		}
 	}
 }
