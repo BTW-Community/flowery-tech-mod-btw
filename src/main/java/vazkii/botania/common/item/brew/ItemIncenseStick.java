d141272c90802b3df0375e9c73f933751ed84b1c
TheWinABagel <ash@rjohnston.net>
2025-10-23 00:20:48 -0500

    oh...

--- src/main/java/vazkii/botania/common/item/brew/ItemIncenseStick.java
+++ src/main/java/vazkii/botania/common/item/brew/ItemIncenseStick.java
@@ -58,8 +58,7 @@ public class ItemIncenseStick extends ItemMod implements IBrewItem, IBrewContain
 
 	@Override
 	public void registerIcons(IconRegister par1IconRegister) {
-		//todofix incense stick rendering should be 2, but crashes with 2. strange
-		icons = new Icon[3];
+		icons = new Icon[2];
 		for(int i = 0; i < icons.length; i++)
 			icons[i] = IconHelper.forItem(par1IconRegister, this, i);
 	}
@@ -71,7 +70,7 @@ public class ItemIncenseStick extends ItemMod implements IBrewItem, IBrewContain
 
 	@Override
 	public int getRenderPasses(int metadata) {
-		return 2;
+		return 1;
 	}
 
 	@Override
