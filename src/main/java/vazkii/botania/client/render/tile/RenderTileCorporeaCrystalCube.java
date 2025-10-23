a781fbdc38beba20d1bfeb89812a7ad9aab874e4
TheWinABagel <ash@rjohnston.net>
2025-10-23 00:38:43 -0500

    cleanup

--- src/main/java/vazkii/botania/client/render/tile/RenderTileCorporeaCrystalCube.java
+++ src/main/java/vazkii/botania/client/render/tile/RenderTileCorporeaCrystalCube.java
@@ -44,10 +44,6 @@ public class RenderTileCorporeaCrystalCube extends TileEntitySpecialRenderer {
 
 		entity.age = ClientTickHandler.ticksInGame;
 		ItemStack stack = cube.getRequestTarget();
-		/*if (stack == null || stack.getItem() == null) {
-//			System.out.println("Attempting to render corporea crystal cube with null item target!");
-			return;
-		}*/
 		if (stack != null)
 			entity.setEntityItemStack(stack);
 
