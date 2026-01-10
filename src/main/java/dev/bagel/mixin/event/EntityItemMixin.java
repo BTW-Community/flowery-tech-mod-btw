4d2099523ad71ea54b3142961946df554ab9445a
TheWinABagel <gogglebobby@gmail.com>
2026-01-10 17:08:52 -0600

    entity lifespan impl

--- src/main/java/dev/bagel/mixin/event/EntityItemMixin.java
+++ src/main/java/dev/bagel/mixin/event/EntityItemMixin.java
@@ -2,19 +2,54 @@ package dev.bagel.mixin.event;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import cpw.mods.fml.common.eventhandler.Event;
-import net.minecraft.src.EntityItem;
-import net.minecraft.src.EntityPlayer;
-import net.minecraft.src.ItemStack;
+import net.minecraft.src.*;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.botania.common.item.ItemFlowerBag;
 import vazkii.botania.common.item.ModItems;
 
 @Mixin(EntityItem.class)
-public class EntityItemMixin {
+public abstract class EntityItemMixin extends Entity {
+    @Shadow
+    public abstract ItemStack getEntityItem();
+
+    @Unique
+    int lifespan = 6000;
+    public EntityItemMixin(World par1World) {
+        super(par1World);
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/src/World;DDDLnet/minecraft/src/ItemStack;)V", at = @At("TAIL"))
+    private void init(World worldIn, double par2, double par4, double par6, ItemStack stack, CallbackInfo ci) {
+        this.lifespan = (stack.getItem() == null ? 6000 : stack.getItem().getEntityLifespan(stack, worldIn));
+    }
+
+    @ModifyConstant(method = "checkForItemDespawn", constant = @Constant(intValue = 6000))
+    private int checkForItemDespawn(int original) {
+        var stack = this.getEntityItem();
+        return (stack.getItem() == null ? 6000 : stack.getItem().getEntityLifespan(stack, this.worldObj));
+    }
+
+    @Inject(method = "readEntityFromNBT", at = @At("TAIL"))
+    private void readEntityFromNBT(NBTTagCompound tagCompund, CallbackInfo ci) {
+        if (tagCompund.hasKey("Lifespan"))
+        {
+            lifespan = tagCompund.getInteger("Lifespan");
+        }
+    }
+
+    @Inject(method = "writeEntityToNBT", at = @At("TAIL"))
+    private void writeEntityToNBT(NBTTagCompound tagCompound, CallbackInfo ci) {
+        tagCompound.setInteger("Lifespan", lifespan);
+    }
+
     @Inject(method = "onCollideWithPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/InventoryPlayer;addItemStackToInventory(Lnet/minecraft/src/ItemStack;)Z"), cancellable = true)
     private void onCollideWithPlayer(EntityPlayer player, CallbackInfo ci) {
         var event = new EntityItemPickupEvent(player, (EntityItem) (Object) this);
