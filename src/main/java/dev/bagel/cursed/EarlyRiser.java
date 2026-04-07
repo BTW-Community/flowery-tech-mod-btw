4b10d7f598063eb48be47faf641e79fae963b976
TheWinABagel <gogglebobby@gmail.com>
2026-04-07 16:04:02 -0500

    Things are remapped in runtime. I forgot.

--- src/main/java/dev/bagel/cursed/EarlyRiser.java
+++ src/main/java/dev/bagel/cursed/EarlyRiser.java
@@ -10,9 +10,26 @@ import org.objectweb.asm.tree.MethodNode;
 public class EarlyRiser implements Runnable {
     @Override
     public void run() {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            System.out.println("EarlyRiser is running!");
-        }
+        System.out.println("EarlyRiser is running!");
+        var resolver = FabricLoader.getInstance().getMappingResolver();
+        //EnumArmorMaterial
+        ClassTinkerers.enumBuilder(resolver.mapClassName("intermediary", "net.minecraft.class_1029"), int.class, int[].class, int.class)
+                .addEnum("MANASTEEL", 16, new int[] { 2, 6, 5, 2 }, 18)
+                .addEnum("ELEMENTIUM", 18, new int[] { 2, 6, 5, 2 }, 18)
+                .addEnum("TERRASTEEL", 34, new int[] {3, 8, 6, 3}, 26)
+                .addEnum("MANAWEAVE", 5, new int[] { 1, 2, 2, 1 }, 18)
+                .build();
+        //EnumToolMaterial
+        ClassTinkerers.enumBuilder(resolver.mapClassName("intermediary", "net.minecraft.class_1070"), int.class, int.class, float.class, float.class, int.class, int.class, int.class)
+                .addEnum("MANASTEEL", 3, 300, 6.2F, 2F, 20, 25, 2)
+                .addEnum("ELEMENTIUM", 3, 720, 6.2F, 2F, 20, 35, 3)
+                .addEnum("TERRASTEEL", 4, 2300, 9F, 3F, 26, 30, 5)
+                .build();
+
+        //EnumRarity
+        ClassTinkerers.enumBuilder(resolver.mapClassName("intermediary", "net.minecraft.class_1081"), int.class, String.class)
+                .addEnum("RELIC", 6, "Relic")
+                .build();
         ClassTinkerers.addTransformation("api.world.data.DataEntry$PlayerDataEntry", classNode -> {
             classNode.access &= ~ Opcodes.ACC_FINAL;
             classNode.access |= Opcodes.ACC_PUBLIC;
@@ -30,21 +47,5 @@ public class EarlyRiser implements Runnable {
             classNode.access &= ~ Opcodes.ACC_FINAL;
             classNode.access |= Opcodes.ACC_PUBLIC;
         });
-        ClassTinkerers.enumBuilder("net.minecraft.src.EnumArmorMaterial", int.class, int[].class, int.class)
-                .addEnum("MANASTEEL", 16, new int[] { 2, 6, 5, 2 }, 18)
-                .addEnum("ELEMENTIUM", 18, new int[] { 2, 6, 5, 2 }, 18)
-                .addEnum("TERRASTEEL", 34, new int[] {3, 8, 6, 3}, 26)
-                .addEnum("MANAWEAVE", 5, new int[] { 1, 2, 2, 1 }, 18)
-                .build();
-
-        ClassTinkerers.enumBuilder("net.minecraft.src.EnumToolMaterial", int.class, int.class, float.class, float.class, int.class, int.class, int.class)
-                .addEnum("MANASTEEL", 3, 300, 6.2F, 2F, 20, 25, 2)
-                .addEnum("B_ELEMENTIUM", 3, 720, 6.2F, 2F, 20, 35, 3)
-                .addEnum("TERRASTEEL", 4, 2300, 9F, 3F, 26, 30, 5)
-                .build();
-
-        ClassTinkerers.enumBuilder("net.minecraft.src.EnumRarity", int.class, String.class)
-                .addEnum("RELIC", 6, "Relic")
-                .build();
     }
 }
