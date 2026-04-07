264012e64f6fbc64877a1db59f36c063ec495eb6
TheWinABagel <gogglebobby@gmail.com>
2026-04-07 12:27:09 -0500

    remove old todo

--- src/main/java/dev/bagel/util/GuiButtonSound.java
+++ src/main/java/dev/bagel/util/GuiButtonSound.java
@@ -11,7 +11,7 @@ public class GuiButtonSound extends GuiButton {
     public GuiButtonSound(int par1, int par2, int par3, int par4, int par5, String par6Str) {
         super(par1, par2, par3, par4, par5, par6Str);
     }
-    //todofix redirect GuiScreen#mouseClicked and check button for GuiButtonSound
+
     /** Play sound*/
     public void func_146113_a() {
         Minecraft.getMinecraft().sndManager.playSoundFX("random.click", 1.0f, 1.0f);
